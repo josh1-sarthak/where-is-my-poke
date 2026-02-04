e2e089419b24d516e7fdf214f385236c9006ce35
Sarthak Joshi <71423676+josh1-sarthak@users.noreply.github.com>
2026-02-04 22:42:38 +0530

    chore: updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,23 @@ const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/where-is-my-poke/",
     clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    historyApiFallback: {
+      index: "/where-is-my-poke/index.html",
+    },
+    open: ["/where-is-my-poke/"],
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -19,7 +30,7 @@ module.exports = {
       patterns: [{ from: "public/images", to: "images" }],
     }),
     new Dotenv({
-      systemvars: true, // This is the magic line!
+      systemvars: true,
     }),
   ],
   module: {
