9947ee679f052a23c4bca8477debee4e0720eded
Sarthak Joshi <71423676+josh1-sarthak@users.noreply.github.com>
2026-02-04 19:24:08 +0530

    chore: refactored assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-            }
-        ]
-    }
-}
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public/images", to: "images" }],
+    }),
+  ],
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+};
